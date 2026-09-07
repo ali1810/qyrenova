5b9f2967945399e855b68fe44cace618e88300c4
Mushtaq Ali <ali10786@gmail.com>
2026-09-08 01:00:03 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const nav = document.querySelector('.nav');
-const menu = document.querySelector('.menu-btn');
-menu?.addEventListener('click', () => nav.classList.toggle('open'));
-document.querySelectorAll('.links a').forEach(a => a.addEventListener('click', () => nav.classList.remove('open')));
